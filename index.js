c5acdaee5f7a12d700162a5d437f7d9a4a1f48e9
Georgios <gkitsikoudis@gmx.de>
2022-10-17 21:20:27 +0200

    Change Hello World to Hello Borld

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ let persons = [
 ]
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World</h1>')
+    response.send('<h1>Hello Borld</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
